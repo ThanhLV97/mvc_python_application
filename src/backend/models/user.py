28282de824a9cb29e48020f79b153165d02157d5
ThanhLV97 <luuvanthanh15dt2@gmail.com>
2023-10-20 22:50:13 +0700

    #3 - Init db with migration, add user, role, user_role model

--- src/backend/models/user.py
+++ src/backend/models/user.py
@@ -2,13 +2,44 @@ from extensions import db
 
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+
+    # Relationship
+    roles = db.relationship("Role", secondary="user_roles", back_populates="users")
 
     def __repr__(self):
         return f"<User {self.username}>"
 
     def to_dict(self):
         return {"id": self.id, "name": self.username, "email": self.email}
+
+    def has_role(self, role):
+        return bool(
+            Role.query
+            .join(Role.users)
+            .filter(User.id == self.id)
+            .filter(Role.slug == role)
+            .count() == 1
+        )
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(36), nullable=False)
+    slug = db.Column(db.String(36), nullable=False, unique=True)
+
+    # Relationshop
+    users = db.relationship("User", secondary="user_roles", back_populates="roles")
+
+
+class UserRole(db.Model):
+    __tablename__ = "user_roles"
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), primary_key=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"), primary_key=True)
